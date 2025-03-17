2db8453803277249758d79ab16dea81e5d0a4ae0
pranalYadav <yadav.pranal94@gmail.com>
2025-03-17 12:39:10 +0530

    image optimization and nested layout

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import {
   CalendarDaysIcon,
   HomeIcon,
@@ -29,13 +30,13 @@ function SideNavigation() {
       <ul className='flex flex-col gap-2 h-full text-lg'>
         {navLinks.map((link) => (
           <li key={link.name}>
-            <a
+            <Link
               className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
               href={link.href}
             >
               {link.icon}
               <span>{link.name}</span>
-            </a>
+            </Link>
           </li>
         ))}
 
