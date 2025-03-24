ef3517467ba85defa14dd56d1fc1abbe6eb74d65
pranalYadav <yadav.pranal94@gmail.com>
2025-03-24 12:43:38 +0530

    added filter component and highlighting link

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,16 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const revalidate = 3600;
 export const metadata = {
     title: "Cabins"
 }
 
-export default function Page() {
+export default function Page({ searchParams }) {
     // CHANGE
+    const filter = searchParams?.capacity ?? "all";
 
     return (
         <div>
@@ -23,8 +25,13 @@ export default function Page() {
                 away from home. The perfect spot for a peaceful, calm vacation. Welcome
                 to paradise.
             </p>
-            <Suspense fallback={<Spinner />}>
-                <CabinList />
+
+            <div className="flex justify-end mb-8">
+                <Filter />
+            </div>
+
+            <Suspense fallback={<Spinner />} key={filter}>
+                <CabinList filter={filter} />
             </Suspense>
         </div>
     );
