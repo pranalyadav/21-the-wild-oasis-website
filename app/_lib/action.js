3f91c6796240783b6378e25b83e9c1b58fb440d5
pranalYadav <yadav.pranal94@gmail.com>
2025-03-28 12:57:55 +0530

    learned about middleware

---
+++ app/_lib/action.js
@@ -0,0 +1,10 @@
+"use server"
+import { signIn, signOut } from "./auth"
+
+export async function SignInAction() {
+    await signIn("google", { redirectTo: "/account" })
+}
+
+export async function SignOutAction() {
+    await signOut({ redirectTo: "/" })
+}
